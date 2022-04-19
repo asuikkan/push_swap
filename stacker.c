4efcc70ac824915e1af4c98be9b80c8d39ea2ff5
Arttu Suikkanen <asuikkan@c1r4p10.hive.fi>
2022-04-19 16:09:55 +0300

    added error handling and freeing

--- stacker.c
+++ stacker.c
@@ -12,13 +12,37 @@
 
 #include "push_swap.h"
 
-static void	error_handler(void)
+static void	error_handler(t_stack *stacks)
 {
+	if (stacks)
+	{
+		if (stacks->a)
+			free(stacks->a);
+		if (stacks->b)
+			free(stacks->b);
+		free(stacks);
+	}
 	write(2, "Error\n", 6);
 	exit(1);
 }
 
-static int	validate_arg(char *arg)
+static t_stack	*initialize(int count)
+{
+	t_stack	*stacks;
+
+	stacks = (t_stack *)malloc(sizeof(t_stack));
+	if (stacks == NULL)
+		error_handler(stacks);
+	stacks->a = ft_memalloc(count);
+	stacks->b = ft_memalloc(count);
+	if (stacks->a == NULL || stacks->b == NULL)
+		error_handler(stacks);
+	stacks->count_a = count;
+	stacks->count_b = 0;
+	return (stacks);
+}
+
+static int	validate_arg(int *integer, char *arg)
 {
 	long	value;
 	int		sign;
@@ -35,13 +59,14 @@ static int	validate_arg(char *arg)
 	while (arg[i])
 	{
 		if (!ft_isdigit(arg[i]))
-			error_handler();
+			return (0);
 		value = value * 10 + (arg[i++] - '0');
 		if ((value == LIMIT && sign == 1) || value > LIMIT)
-			error_handler();
+			return (0);
 	}
 	value *= sign;
-	return (value);
+	*integer = value;
+	return (1);
 }
 
 void	create_stacks(int count, char **argv)
@@ -49,19 +74,12 @@ void	create_stacks(int count, char **argv)
 	t_stack	*stacks;
 	int		i;
 
-	stacks = (t_stack *)malloc(sizeof(t_stack));
-	if (stacks == NULL)
-		exit(1);
-	stacks->a = ft_memalloc(count);
-	stacks->b = ft_memalloc(count);
-	if (stacks->a == NULL || stacks->b == NULL)
-		exit(1);
-	stacks->count_a = count;
-	stacks->count_b = 0;
+	stacks = initialize(count);
 	i = 1;
 	while (--count >= 0)
 	{
-		stacks->a[count] = validate_arg(argv[i++]);
+		if (!validate_arg(&stacks->a[count], argv[i++]))
+			error_handler(stacks);
 		ft_printf("stack_a[%d] = %d\n", count, stacks->a[count]);
 	}
 }
